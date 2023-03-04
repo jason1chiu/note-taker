d322036e284302ca4276eb6ab2d3ebf4aca6f0b0
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-03 23:39:32 -0500

    added comments to the nodes and express

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Import the necessary modules for creating the application and working with files
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const {v4: uuid} = require('uuid');
 
+// Create a new Express application instance
 const app = express();
